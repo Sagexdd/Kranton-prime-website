52ed916fa5f5be340402f9024bf201e469936bb7
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-04-04 14:12:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (!response.ok) throw new Error("Failed to fetch data");
             
             const data = await response.json();
-            const botID = "YOUR_BOT_ID"; // Replace with your bot's ID
+            const botID = "1330071725659918377"; // Replace with your bot's ID
             
             // Check if the bot is in the members list
             const isOnline = data.members.some(member => member.id === botID);
